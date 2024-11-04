9549039f75f1a5f79469d157598f4b31470256b1
Quoc Vu <quocvu2640@gmail.com>
2024-11-04 12:14:21 -0500

    feat: added delete functionality
    removed extra component files and rebased it into the BookTable component.

--- client/src/Components/BookTable.jsx
+++ client/src/Components/BookTable.jsx
@@ -1,30 +1,46 @@
-import { Button, Table } from "flowbite-react";
+import { Modal, Table, Dropdown, Alert } from "flowbite-react";
+import { HiOutlineTrash, HiDownload, HiInformationCircle } from "react-icons/hi";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { RowDropDown } from "./RowDropDown";
 
 export function BookTable() {
-
+    // Book data
     const [books, setBooks] = useState([])
 
-    // GET request to /books to retrieve all book data.
-    useEffect(() => {
-        const fetchData = async () => {
-            await axios.get('http://localhost:5050/books/')
-                .then(res => {
-                    console.log(res);
-                    setBooks(res.data)
-                })
-                .catch(err => {
-                    console.log(err.response)
-                })
-        }
-        fetchData()
-    }, [])
+    // State to check if reload is necessary.
+    const [reload, setReload] = useState(true)
 
+    // Notification states.
+    const [isNotification, setIsNotification] = useState(false)
+    const [response, setResponse] = useState([])
+
+    // GET request to /books to retrieve all book data.
+    // Refreshes after a delete.
+    useEffect(() => {
+        if (reload) {
+            const fetchData = async () => {
+                await axios.get('http://localhost:5050/books/')
+                    .then(res => {
+                        console.log(res);
+                        setBooks(res.data)
+                    })
+                    .catch(err => {
+                        console.log(err.response)
+                    })
+            }
+            fetchData()
+        }
+        setReload(false)
+
+    }, [reload]) // Reloads on state change.
 
     return (
-        <div className="overflow-x-auto">
+        <div className="">
+            <Modal show={isNotification} >
+                <Alert color={response["status"] === 204 ? "info" : "failure"} icon={HiInformationCircle} onDismiss={() => { setIsNotification(false); setReload(true) }}>
+                    <span className="font-medium">Info: </span> Delete successful!
+                </Alert>
+            </Modal>
             <Table striped>
                 <Table.Head>
                     <Table.HeadCell>Title</Table.HeadCell>
@@ -46,12 +62,47 @@ export function BookTable() {
                             <Table.Cell>{book['pubDate']}</Table.Cell>
                             <Table.Cell>{book['id']}</Table.Cell>
                             <Table.Cell>
-                                <RowDropDown id={book['id']}></RowDropDown>
+                                <BookRowDropDown id={book['id']}
+                                    setIsNotification={setIsNotification}
+                                    setResponse={setResponse}></BookRowDropDown>
                             </Table.Cell>
                         </Table.Row>
                     ))}
                 </Table.Body>
             </Table>
         </div>
+    );
+}
+
+// Dropdown component for book row.
+const BookRowDropDown = (props) => {
+    const { id, setIsNotification, setResponse } = props
+
+    const openInNewTab = (url) => {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+    }
+
+    const deleteBookRequest = async (e) => {
+        setIsNotification(true)
+        await axios.delete('http://localhost:5050/books/' + id)
+            .then(res => {
+                console.log(res)
+                setResponse(res)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+    }
+
+    return (
+        <>
+            <Dropdown label="" inline>
+                <Dropdown.Item icon={HiDownload} onClick={() => openInNewTab('http://localhost:5050/books/' + id + '/cover')}>Cover</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item icon={HiOutlineTrash} onClick={deleteBookRequest}>Delete</Dropdown.Item>
+            </Dropdown>
+        </>
     );
 }
